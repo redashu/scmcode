423eedfe7daa9e300dcf1113cc893ec01fcc7377
redashu <ashutoshh@linux.com>
2018-03-20 15:45:28 +0000

    day 1 done

---
+++ abc.php
@@ -0,0 +1,7 @@
+<?php
+$v=1000;
+echo "Hello world \n";
+echo  $v;
+echo  "\n";
+
+?>
---
+++ hw.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def  hww():
+    print("Hello world")
+
+
+
+
+if __name__ == "__main__" :
+    hww()
